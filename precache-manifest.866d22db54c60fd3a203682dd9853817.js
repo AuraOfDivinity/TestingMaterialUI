f9c59843fce462ef0d0a1ec6c80aa31bca1b51e6
Auraofdivinity <peirisasel373@gmail.com>
2019-09-10 07:14:19 +0530

    Updates

---
+++ precache-manifest.866d22db54c60fd3a203682dd9853817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776d1ca9ae1c0e7a9891a04e5508f72b",
+    "url": "/TestingMaterialUI/index.html"
+  },
+  {
+    "revision": "40747c7e64c65bf20205",
+    "url": "/TestingMaterialUI/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "117f012c3f7114c51349",
+    "url": "/TestingMaterialUI/static/js/2.c30f2232.chunk.js"
+  },
+  {
+    "revision": "40747c7e64c65bf20205",
+    "url": "/TestingMaterialUI/static/js/main.849eb758.chunk.js"
+  },
+  {
+    "revision": "3ba17ce558a2d541a9bb",
+    "url": "/TestingMaterialUI/static/js/runtime~main.fdccf2d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestingMaterialUI/static/media/logo.5d5d9eef.svg"
+  }
+]);
